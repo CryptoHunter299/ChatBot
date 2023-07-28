e23963b58a8c51ddf73cc26255c3d367f526fb2b
CryptoHunter299 <96936412+CryptoHunter299@users.noreply.github.com>
2023-07-28 20:11:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import json
 from glob import glob
 import pytz
 from datetime import datetime
-from config import TOKEN, ADMIN, OWNER, CHANNEL, GROUP, PROJECT_NAME
+from config import TOKEN, ADMIN, OWNER, CHANNEL, GROUP, GROUP2, PROJECT_NAME
 
 token = TOKEN
 bot = amanobot.Bot(token)
